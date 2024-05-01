a9bd35987345aad7c971deed1d80c095f3c9b6bf
DrDisagree <crazymahmud08@gmail.com>
2024-05-02 03:05:47 +0600

    Bump dependencies and add location dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -148,6 +148,9 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.lifecycle.runtime.compose)
 
+    // Accompanist permissions
+    implementation(libs.google.accompanist.permissions)
+
     // Text Flow
     implementation(libs.textflow)
 }
