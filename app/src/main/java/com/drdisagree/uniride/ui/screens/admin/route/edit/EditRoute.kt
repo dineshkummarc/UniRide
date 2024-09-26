8e72539943583a1d34d1fa214d9f5f3caa8a2bd9
DrDisagree <crazymahmud08@gmail.com>
2024-09-27 01:09:11 +0600

    Update route deletion cancel button style

--- app/src/main/java/com/drdisagree/uniride/ui/screens/admin/route/edit/EditRoute.kt
+++ app/src/main/java/com/drdisagree/uniride/ui/screens/admin/route/edit/EditRoute.kt
@@ -23,6 +23,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -433,7 +434,7 @@ private fun EditRouteFields(
                 }
             },
             dismissButton = {
-                Button(
+                TextButton(
                     onClick = {
                         onCloseDialog()
                     }) {
