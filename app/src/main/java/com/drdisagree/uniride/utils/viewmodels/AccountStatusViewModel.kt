cbeaf312f46201230a101170e265423ee152baef
DrDisagree <crazymahmud08@gmail.com>
2024-10-01 19:05:05 +0600

    Check admin status in realtime

--- app/src/main/java/com/drdisagree/uniride/utils/viewmodels/AccountStatusViewModel.kt
+++ app/src/main/java/com/drdisagree/uniride/utils/viewmodels/AccountStatusViewModel.kt
@@ -1,15 +1,13 @@
 package com.drdisagree.uniride.utils.viewmodels
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.drdisagree.uniride.data.utils.Constant.ADMIN_COLLECTION
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ListenerRegistration
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
 @HiltViewModel
@@ -21,6 +19,8 @@ class AccountStatusViewModel @Inject constructor(
     private val _isAdmin = MutableStateFlow<Boolean?>(null)
     val isAdmin = _isAdmin.asStateFlow()
 
+    private var adminStatusListener: ListenerRegistration? = null
+
     init {
         fetchUserAdminStatus(firebaseAuth.currentUser?.uid)
     }
@@ -28,21 +28,22 @@ class AccountStatusViewModel @Inject constructor(
     private fun fetchUserAdminStatus(userId: String?) {
         if (userId == null) return
 
-        viewModelScope.launch {
-            try {
-                val isAdmin = isUserAdmin(userId)
-                _isAdmin.emit(isAdmin)
-            } catch (ignored: Exception) {
-                _isAdmin.emit(false)
+        adminStatusListener?.remove()
+
+        adminStatusListener = firestore.collection(ADMIN_COLLECTION)
+            .document(userId)
+            .addSnapshotListener { snapshot, error ->
+                if (error != null) {
+                    _isAdmin.value = false
+                    return@addSnapshotListener
+                }
+
+                _isAdmin.value = snapshot?.exists()
             }
-        }
     }
 
-    private suspend fun isUserAdmin(userId: String): Boolean {
-        return firestore.collection(ADMIN_COLLECTION)
-            .document(userId)
-            .get()
-            .await()
-            .exists()
+    override fun onCleared() {
+        super.onCleared()
+        adminStatusListener?.remove()
     }
 }
